99f28cbcd036e5ea91974e7d5713d827318c6ff7
Stimarc <124087165+Stimarc@users.noreply.github.com>
2023-11-16 00:44:04 +0200

    shop cart add color

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,26 +1,23 @@
-export const Product = ({ data , addToCartHandler }) => {
-    const {id, title, price, img } = data; 
- 
-    const clickHandler= (id) => {
-     addToCartHandler(id);
-    }
- 
-     return (
-         <div className="product">
-         <div className="product__img">
-           <img src={process.env.PUBLIC_URL + `/img/products/${img}`} alt="" />
-           </div>
-           <h4 className='product__title'>{ title }</h4>
-           <div className="product__price-block">
-             <span className="product__price">{ price }</span>
- 
-             <button
-              onClick={() => clickHandler(id) }
-              className="product__buy"
-             >
-               buy
-             </button>
-           </div>
-       </div>
-     );
- };
+export const Product = ({ data, addToCartHandler }) => {
+  const { id, title, price, img } = data;
+
+  const clickHandler = () => {
+    addToCartHandler(data);
+  };
+
+  return (
+    <div className="product">
+      <div className="product__img">
+        <img src={process.env.PUBLIC_URL + `/img/products/${img}`} alt="" />
+      </div>
+      <h4 className="product__title">{title}</h4>
+      <div className="product__price-block">
+        <span className="product__price">{price}</span>
+
+        <button onClick={clickHandler} className="product__buy">
+          buy
+        </button>
+      </div>
+    </div>
+  );
+};
